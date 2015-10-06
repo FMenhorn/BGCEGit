7068a82e31eaf0a61045f30f66d2df2d3d1fb8d7
Friedrich Menhorn <menhorn@in.tum.de>
2015-10-06 16:35:53 +0200

    added dummy main for cadtovoxel and cmakelistfilestuff

---
+++ Prototypes/OpenCascade/CADToVoxel.cpp
@@ -0,0 +1,19 @@
+/*
+ * CADtoVoxel.cpp
+ *
+ *  Created on: Oct 6, 2015
+ *      Author: BGCE
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main(void){
+	return EXIT_SUCCESS;
+}
+
+
+
+
