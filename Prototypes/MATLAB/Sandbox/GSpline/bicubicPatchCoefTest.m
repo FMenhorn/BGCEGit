e38ad5ec3dd3f3f04a02e85b9854adda0171bd23
EWannerberg <daplff@gmail.com>
2015-10-09 13:12:34 +0200

    added test for coefs, works

---
+++ Prototypes/MATLAB/Sandbox/GSpline/bicubicPatchCoefTest.m
@@ -0,0 +1,22 @@
+%% script for testing that the coefs from function are same as coefs used in scheme(and getBicubicPatchIndex)
+
+extraOrdCorner;
+
+patch_test = zeros(3,4,4);
+[Acoef,B1coefs,B2coefs,Ccoefs] = createBicubicCoefMatrices(m);
+
+shifted_indices = @(ind,modul) mod_index((0:(modul-1)) + ind,modul);
+indShift = shifted_indices(m,m);
+
+for j = 1:4
+    for i = 1:4
+        for p = 1:m
+            patch_test(:,i,j) = patch_test(:,i,j) + ...
+                Acoef(i,j) * As(:,m)/m + ...
+                B1coefs(p,i,j) * Bs(:,1,indShift(p)) + ...
+                B2coefs(p,i,j) * Bs(:,2,indShift(p)) + ...
+                Ccoefs(p,i,j) * Cs(:,indShift(p));
+        end
+    end
+end
+patch - patch_test
