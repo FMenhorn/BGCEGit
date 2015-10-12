8fd9ac79b1c509269209a41dd249e9a944d9ee73
YurovaA <yurova92@gmail.com>
2015-10-12 15:17:55 +0200

    sandboxing Doo Sabin

--- Prototypes/MATLAB/Sandbox/PetersScheme/DooSabin.m
+++ Prototypes/MATLAB/Sandbox/PetersScheme/DooSabin.m
@@ -14,8 +14,12 @@ faces_refined = [];
 edges = [];
 counter = 0;
 colours = [];
+n = 3;
 %create an array  of edges
 for i = 1:size(faces,1)
+    if (nnz(faces(i, :)) > n)
+        n = nnz(faces(i, :));
+    end
     if (size(edges,1) == 0)
         for j=0:3
             new_edge = [faces(i, j+1) faces(i, mod(j+1,4)+1)];
@@ -35,37 +39,42 @@ for i = 1:size(faces,1)
     end
 end
 
-edges(1,:);
-verts_children = zeros(N, 4);
+verts_children = zeros(N, 7);%[]; %zeros(N, 4);
+verts_children_faces = zeros(N, 7);
 edges_children = zeros(size(edges,1), 4);
-
+%verts_children = [];
+%edges_children = [];
 %Loop through faces
 for j=1:size(faces,1)
     
     %Determine the coordinates of current face vertices
-    X = vertices(faces(j, :),1);
-    Y = vertices(faces(j, :),2);
-    Z = vertices(faces(j, :),3);
+    n_local = nnz(faces(j, :));
+    X = vertices(faces(j, 1:n_local),1);
+    Y = vertices(faces(j, 1:n_local),2);
+    Z = vertices(faces(j, 1:n_local),3);
     
     %Find the centroid of the current face
     F = [mean(X); mean(Y); mean(Z)];
     
-    %loop through vertices of the current face and create their children
-    vertices_refined(4*j - 3:4*j, 1) = (1 - alpha).*X + alpha.*F(1).*ones(1, length(X))';
-    vertices_refined(4*j - 3:4*j, 2) = (1 - alpha).*Y + alpha.*F(2).*ones(1, length(X))';
-    vertices_refined(4*j - 3:4*j, 3) = (1 - alpha).*Z + alpha.*F(3).*ones(1, length(X))';
+    %loop through vertices of the current face and create their children 
+    vertices_refined(n*j - n + 1:n*j, 1) = (1 - alpha).*X + alpha.*F(1).*ones(1, length(X))';
+    vertices_refined(n*j - n + 1:n*j, 2) = (1 - alpha).*Y + alpha.*F(2).*ones(1, length(X))';
+    vertices_refined(n*j - n + 1:n*j, 3) = (1 - alpha).*Z + alpha.*F(3).*ones(1, length(X))';
     
-    for k = 0:n-1
+    for k = 0:n_local-1
 
         ind = 1;
-        while (verts_children(faces(j,k+1), ind) ~= 0 )
-            ind = ind+1;
+        if size(verts_children,1) ~= 0
+            while (verts_children(faces(j,k+1), ind) ~= 0 )
+                ind = ind+1;
+            end
         end
-        verts_children(faces(j, k+1), ind) = 4*j-3+k;
+        verts_children(faces(j, k+1), ind) = n*j-n+k+1;
+        verts_children_faces(faces(j, k+1), ind) = j;
     end
-    for k = 0:n-1
+    for k = 0:n_local-1
         clear ind1 ind2
-        current_edge = [faces(j, k+1) faces(j, mod(k+1,4)+1)];
+        current_edge = [faces(j, k+1) faces(j, mod(k+1,n)+1)];
         ind1 = find(edges(:,1) == current_edge(1));
         ind2 = find(edges(:,2) == current_edge(1));
       
@@ -79,38 +88,75 @@ for j=1:size(faces,1)
         while (edges_children(current_edge_id, ind) ~= 0 )
             ind = ind+1;
         end
-        edges_children(current_edge_id, ind) = 4*j-3+k;
-        edges_children(current_edge_id, ind+1) = 4*j -4 + mod(k+1,4) + 1;
+        edges_children(current_edge_id, ind) = n*j-n + k + 1;
+        edges_children(current_edge_id, ind+1) = n*j -n + mod(k+1,n) + 1;
         
     end
     
     %add the face formed by children vertices of the old face j
-    faces_refined(j, :) = 4*j-3:4*j;
+    faces_refined(j, 1:n_local) = n*j-n+1:n*j;
     colours(j) = 1;
 end
+%verts_children
 
 %loop through old vertices
 counter = size(faces,1);
 for i = 1:N
-    new_face = verts_children(i, :);
-    if (length(find(new_face)) == 4)
-        
-        vec1 = vertices_refined(new_face(2),:)-vertices_refined(new_face(1),:);
-        vec2 = vertices_refined(new_face(3),:)-vertices_refined(new_face(2),:);
-        vec3 = vertices_refined(new_face(4),:)-vertices_refined(new_face(3),:);
-        
-        normal_1 = cross(vec1, vec2);
-        normal_2 = cross(vec2, vec3);
-        if (sum(normal_1.*normal_2) < 0)
-            temp = new_face(3);
-            new_face(3) = new_face(4);
-            new_face(4) = temp;
+
+    new_face = verts_children(i, (1:nnz(verts_children(i, :))));
+    neighbouring_faces = verts_children_faces(i, (1:nnz(verts_children_faces(i, :))));
+    face_ordered = [new_face(1)];
+    if (length(new_face) >= 3)
+        %find the face adjacent to the face, containing the first child
+        k = 1;
+        for iter = 1:length(new_face)-1
+
+            for j = 2:length(neighbouring_faces)
+                %bad smell. this check is false when we compare the face to
+                %itsel. In the final version there shouldn't be any
+                %comparissons with itself at all, of course
+              
+                if (length(intersect(faces(neighbouring_faces(k),:),faces(neighbouring_faces(j),:))) == 2) && (isempty(find(face_ordered == new_face(j))))
+                    face_ordered = [face_ordered, new_face(j)];
+                    k = j;
+                end
+                iter = iter + 1;
+            end
+
         end
-         counter = counter + 1;
-    
-        faces_refined(counter,:) = new_face;
+        counter = counter + 1;
+        faces_refined(counter,1:length(new_face)) = face_ordered;
         colours(counter) = 2;
     end
+    
+    
+%     X = vertices_refined(new_face, 1)';
+%     Y = vertices_refined(new_face, 2)';
+%     Z = vertices_refined(new_face, 3)';
+%     if (length(new_face) >= 3)
+%         new_face_ordered = convhull(X, Y, Z);
+%         counter = counter + 1;
+%         faces_refined = new_face(new_face_ordered);
+%         colours(counter) = 2;
+%     end
+%     if (length(find(new_face)) == 4)
+%         
+%         vec1 = vertices_refined(new_face(2),:)-vertices_refined(new_face(1),:);
+%         vec2 = vertices_refined(new_face(3),:)-vertices_refined(new_face(2),:);
+%         vec3 = vertices_refined(new_face(4),:)-vertices_refined(new_face(3),:);
+%         
+%         normal_1 = cross(vec1, vec2);
+%         normal_2 = cross(vec2, vec3);
+%         if (sum(normal_1.*normal_2) < 0)
+%             temp = new_face(3);
+%             new_face(3) = new_face(4);
+%             new_face(4) = temp;
+%         end
+%          counter = counter + 1;
+%     
+%         faces_refined(counter,:) = new_face;
+%         colours(counter) = 2;
+%     end
 end
 
 %loop through old edges
@@ -132,7 +178,7 @@ for i=1:size(edges, 1)
         end
          counter = counter + 1;
     
-        faces_refined(counter,:) = new_face;
+        faces_refined(counter,1:4) = new_face;
         colours(counter) = 3;
     end
 end
--- Prototypes/MATLAB/Sandbox/PetersScheme/Torus_refinement.m
+++ Prototypes/MATLAB/Sandbox/PetersScheme/Torus_refinement.m
@@ -4,19 +4,23 @@ close all
 a=5;
 c=10;
 [u,v]=meshgrid(0:10:370);
-%torus equations
-%size_v = size(v);
-x=(c+a*cosd(v)).*cosd(u);
-y=(c+a*cosd(v)).*sind(u);
-z=a*sind(v);
-S = surfl(x,y,z);
-hold off
-%convert our surface to patch
-[faces, verts, facevertexdata] = surf2patch(S);
+% %torus equations
+% size_v = size(v);
+% x=(c+a*cosd(v)).*cosd(u);
+% y=(c+a*cosd(v)).*sind(u);
+% z=a*sind(v);
+% S = surfl(x,y,z);
+% hold off
+% %convert our surface to patch
+% [faces, verts, facevertexdata] = surf2patch(S);
+[X, Y, Z] = sphere
+s = surf(X, Y, Z);
+[faces, verts, facevertexdata] = surf2patch(s);
 
 %set up an averaging parameter alpha
 alpha = 0.5;
 [verts_new, faces_new, colours] = DooSabin(verts, faces, alpha);
+%[verts_new2, faces_new2, colours_new] = DooSabin(verts_new, faces_new, alpha);
 %colours
 
 %plot refined torus
