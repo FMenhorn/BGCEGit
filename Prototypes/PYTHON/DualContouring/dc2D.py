cb504016c21de679d5802e7e457e58b4593b9ad2
Benjamin Rüth <benjamin.rueth@tum.de>
2015-10-15 15:39:39 +0200

    added correct manifold treatment to DC for 2D, some restructuring

--- Prototypes/PYTHON/DualContouring/dc2D.py
+++ Prototypes/PYTHON/DualContouring/dc2D.py
@@ -6,18 +6,43 @@ import numpy.linalg as la
 import scipy.optimize as opt
 import itertools as it
 
-# Cardinal directions
-from skimage.morphology.selem import cube
 
+class PseudoNode:
+    def __init__(self, _voxel_coord, _quad_signs, _dataset, _resolution):
+        self.voxel_coord = _voxel_coord
+        self.res = _resolution
+        self.x = _voxel_coord[0] + .5*self.res
+        self.y = _voxel_coord[1] + .5*self.res
+        value_key = tuple(self.voxel_coord + .5*_resolution * np.array([1.0, 1.0]))
+        self.middle_sign = _dataset[value_key] > 0
+        self.quad_signs = _quad_signs
+        self.connects = self.calculate_connection()
+
+    def calculate_connection(self):
+        connects = []
+        neighbor_keys = [tuple(self.voxel_coord + self.res * np.array([1.0,0.0])),
+                         tuple(self.voxel_coord + self.res * np.array([0.0,1.0])),
+                         tuple(self.voxel_coord + self.res * np.array([-1.0,0.0])),
+                         tuple(self.voxel_coord + self.res * np.array([0.0,-1.0]))]
+        n = self.quad_signs.__len__()
+        for i in range(n):
+            if self.middle_sign != self.quad_signs[i]:
+                connects.append((neighbor_keys[i],neighbor_keys[(i+1)%n]))
+
+        return connects
+
+# Cardinal directions
 dirs = [np.array([1.0, 0.0]), np.array([0.0, 1.0])]
 
 # Vertices of quad
-quad_verts = [np.array([x, y])
-                   for x in np.arange(0,2,1.0)
-                   for y in np.arange(0,2,1.0)]
+#quad_verts = [np.array([x, y])
+#                   for x in np.arange(0.0,2,1.0)
+#                   for y in np.arange(0.0,2,1.0)]
+quad_verts = [np.array([0.0, 0.0]), np.array([1.0, 0.0]), np.array([1.0, 1.0]), np.array([0.0, 1.0])] # traverse verts in CCW manner
 
 # Edges of quad
-quad_edges = [[0,1],[0,2],[1,3],[2,3]]
+#quad_edges = [[0,1],[0,2],[1,3],[2,3]]
+quad_edges = [[0,1],[1,2],[2,3],[3,0]] # traverse edges in CCW manner
 
 # Use non-linear root finding to compute intersection point
 def estimate_hermite(data, v0, v1):
@@ -29,23 +54,35 @@ def estimate_hermite(data, v0, v1):
 
 
 def tworesolution_dual_contour(dataset,resolutions,dims):
-    [dc_verts_fine,dc_edges_fine]=dual_contour(dataset,resolutions['fine'],dims)
-    [dc_verts_coarse,dc_edges_coarse]=dual_contour(dataset,resolutions['coarse'],dims)
+    print "DC FINE"
+    [dc_verts_fine, dc_edges_fine, dc_verts_pseudo_fine]=dual_contour(dataset,
+                                                                      resolutions['fine'],
+                                                                      dims,
+                                                                      coarse_level = False)
+    print "DC COARSE"
+    [dc_verts_coarse, dc_edges_coarse,dc_verts_pseudo_coarse]=dual_contour(dataset,
+                                                                           resolutions['coarse'],
+                                                                           dims,
+                                                                           coarse_level = True)
 
-    dc_verts={'fine':dc_verts_fine,'coarse':dc_verts_coarse}
-    dc_edges={'fine':dc_edges_fine,'coarse':dc_edges_coarse}
+    dc_verts={'fine':dc_verts_fine, 'coarse':dc_verts_coarse}
+    dc_edges={'fine':dc_edges_fine, 'coarse':dc_edges_coarse}
+    dc_verts_pseudo={'fine':dc_verts_pseudo_fine, 'coarse':dc_verts_pseudo_coarse}
 
-    return dc_verts, dc_edges
+    return dc_verts, dc_edges, dc_verts_pseudo
 
 
 # Input:
 # data = voxel data
 # res = resolution
 # dims = dimension of data
-def dual_contour(data,res,dims):
+def dual_contour(data, res, dims, coarse_level):
     # Compute vertices
     dc_verts = []
     vindex = {}
+    dc_pseudo_verts = []
+    pseudo_index = {}
+
     for x, y in it.product(np.arange(dims['xmin'], dims['xmax'], res), np.arange(dims['ymin'], dims['ymax'], res)):
         o = np.array([float(x), float(y)])
 
@@ -60,6 +97,15 @@ def dual_contour(data,res,dims):
 
         if all(quad_signs) or not any(quad_signs):
             continue
+
+        #this checks for certain problematic cases and introduces pseudo nodes, if necessary
+        if coarse_level:
+            if quad_signs == [False, True, False, True] or quad_signs == [True, False, True, False]:
+                #print quad_signs
+                key = tuple(o)
+                pseudo_index[key] = len(dc_pseudo_verts)
+                dc_pseudo_verts.append(PseudoNode(o, quad_signs, data, res))
+                continue
 
         # Estimate hermite data
         h_data = []
@@ -81,87 +127,30 @@ def dual_contour(data,res,dims):
             continue
 
         # Emit one vertex per every cube that crosses
-        vindex[tuple(o)] = len(dc_verts)
+        key = tuple(o)
+        vindex[key] = len(dc_verts)
         dc_verts.append(v)
 
     # Construct faces
     dc_edges = []
     for x, y in it.product(np.arange(dims['xmin'], dims['xmax'], res), np.arange(dims['ymin'], dims['ymax'], res)):
-        if not (x, y) in vindex:
+        # Emit one edge per each edge that crosses
+        if (x, y) in vindex:
+            o = np.array([float(x), float(y)])
+            for i in range(2):
+                if tuple(o + res*dirs[i]) in vindex:
+                    if (data[tuple(o+res*dirs[i]+res*dirs[int(not i)])] > 0) != (data[tuple(o+res*dirs[i])] > 0):
+                        dc_edges.append([vindex[tuple(o)], vindex[tuple(o + np.array(dirs[i])*res)]])
+        elif (x, y) in pseudo_index and coarse_level:
+            #print pseudo_index
+            #print "found pseudo!"
+            o = np.array([float(x), float(y)])
+            key = tuple(o)
+            i = pseudo_index[key]
+            print "pseudo node at "+str(o)+":"
+            for c in dc_pseudo_verts[i].connects:
+                dc_edges.append([vindex[c[0]], vindex[c[1]]])
+        else:
             continue
 
-        # Emit one edge per each edge that crosses
-        o = np.array([float(x), float(y)])
-        for i in range(2):
-            if tuple(o + res*dirs[i]) in vindex:
-                if (data[tuple(o+res*dirs[i]+res*dirs[int(not i)])] > 0) != (data[tuple(o+res*dirs[i])] > 0):
-                    dc_edges.append([vindex[tuple(o)], vindex[tuple(o + np.array(dirs[i])*res)]])
-
-    return dc_verts, dc_edges
-
-
-def export_as_csv(data,name):
-    import csv
-    with open(name+'.csv', 'wb') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=';',
-                           quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for d in data:
-            csvwriter.writerow(np.array(d))
-
-
-def sample_data(f, res, dims):
-    data = {}
-    for x, y in it.product(np.arange(dims['xmin'], dims['xmax']+res, res), np.arange(dims['ymin'], dims['ymax']+res, res)):
-        key = (x, y)
-        x_vec = np.array([x, y])
-        data[key] = f(x_vec)
-    return data
-
-
-def data_to_voxel(_data, res, dims):
-    length = (dims['xmax']-dims['xmin'])
-    height = (dims['ymax']-dims['ymin'])
-
-    voxels_mat = np.empty([length/res+1,height/res+1])
-    x_mat = np.empty([length/res+1,height/res+1])
-    y_mat = np.empty([length/res+1,height/res+1])
-    for i in range(int(length/res+1)):
-        for j in range(int(height/res+1)):
-            thisX = dims['xmin']+i*res
-            thisY = dims['ymin']+j*res
-            voxels_mat[i,j]=int(_data[tuple(np.array([thisX,thisY]))] > 0)
-            x_mat[i,j] = thisX
-            y_mat[i,j] = thisY
-
-    return voxels_mat,x_mat,y_mat
-
-
-def test_f(x):
-    center = np.array([4.0, 4.0])
-    radius = 2.0
-    d = x - center
-    return np.dot(d, d) - radius ** 2
-
-
-dimensions = {'xmin':0,'xmax':8,'ymin':0,'ymax':8}
-res_fine = 1.0/2.0
-res_coarse = res_fine * 4.0
-resolutions = {'fine':res_fine,'coarse':res_coarse}
-
-data = sample_data(test_f,resolutions['fine'],dimensions)
-[voxels_fine,x_mat_fine,y_mat_fine] = data_to_voxel(data,resolutions['fine'],dimensions)
-[voxels_coarse,x_mat_coarse,y_mat_coarse] = data_to_voxel(data,resolutions['coarse'],dimensions)
-
-[verts, edges] = tworesolution_dual_contour(data, resolutions, dimensions)
-
-export_as_csv(verts['fine'],'verts_fine')
-export_as_csv(edges['fine'],'edges_fine')
-export_as_csv(voxels_fine,'voxels_fine')
-export_as_csv(x_mat_fine,'x_mat_fine')
-export_as_csv(y_mat_fine,'y_mat_fine')
-
-export_as_csv(verts['coarse'],'verts_coarse')
-export_as_csv(edges['coarse'],'edges_coarse')
-export_as_csv(voxels_coarse,'voxels_coarse')
-export_as_csv(x_mat_coarse,'x_mat_coarse')
-export_as_csv(y_mat_coarse,'y_mat_coarse')
+    return np.array(dc_verts), np.array(dc_edges), np.array(dc_pseudo_verts)
---
+++ Prototypes/PYTHON/DualContouring/example3D.py
@@ -0,0 +1,51 @@
+from dc3D import tworesolution_dual_contour
+from dcSample import sample_data, sphere_f, doubletorus_f, torus_f
+
+import numpy as np
+
+dimensions = {'xmin': 0.0, 'xmax': 8.0, 'ymin': 0.0, 'ymax': 8.0, 'zmin': 0.0, 'zmax': 8.0}
+res_fine = .5
+res_coarse = res_fine * 4.0
+resolutions = {'fine': res_fine,'coarse': res_coarse}
+
+fine_data = sample_data(torus_f, resolutions['fine'], dimensions)
+[verts_out_dc, quads_out_dc] = tworesolution_dual_contour(fine_data, resolutions, dimensions)
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.set_aspect('equal')
+
+for q in quads_out_dc['coarse']:
+    vtx = verts_out_dc['coarse'][q]
+    x = vtx[:,0].tolist()
+    y = vtx[:,1].tolist()
+    z = vtx[:,2].tolist()
+    vtx = [zip(x,y,z)]
+    poly=Poly3DCollection(vtx)
+    poly.set_color('b')
+    poly.set_edgecolor('k')
+    poly.set_alpha(.25)
+    ax.add_collection3d(poly)
+
+for q in quads_out_dc['fine']:
+    vtx = verts_out_dc['fine'][q]
+    x = vtx[:,0].tolist()
+    y = vtx[:,1].tolist()
+    z = vtx[:,2].tolist()
+    vtx = [zip(x,y,z)]
+    poly=Poly3DCollection(vtx)
+    poly.set_color('r')
+    poly.set_edgecolor('k')
+    poly.set_alpha(.25)
+    ax.add_collection3d(poly)
+
+ax.set_xlim3d(2, 6)
+ax.set_ylim3d(2, 6)
+ax.set_zlim3d(2, 6)
+plt.show()
+
