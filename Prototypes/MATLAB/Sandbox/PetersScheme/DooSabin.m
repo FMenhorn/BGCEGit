c8b3cbad00d3321efc501a243c5da4e6e46c28e6
YurovaA <yurova92@gmail.com>
2015-10-15 13:11:33 +0200

    sandboxing doo sabin

--- Prototypes/MATLAB/Sandbox/PetersScheme/DooSabin.m
+++ Prototypes/MATLAB/Sandbox/PetersScheme/DooSabin.m
@@ -22,30 +22,34 @@ for i = 1:size(faces,1)
     if (nnz(faces(i, :)) > n)
         n = nnz(faces(i, :));
     end
+    n_local = nnz(faces(i, :));
     if (size(edges,1) == 0)
-        for j=0:3
-            new_edge = [faces(i, j+1) faces(i, mod(j+1,4)+1)];
+        for j=0:n_local - 1
+            new_edge = [faces(i, j+1) faces(i, mod(j+1,n_local-1)+1)];
             counter = counter + 1;
             edges(counter,:) = new_edge;
         end
     else
-        for j=0:3
-            new_edge = [faces(i, j+1) faces(i, mod(j+1,4)+1)];
+        for j=0:n_local - 1
+            new_edge = [faces(i, j+1) faces(i, mod(j+1,n_local)+1)];
             ind1 = find(edges(:,1) == new_edge(1));
             ind2 = find(edges(:,2) == new_edge(1));
-            if ((size(find(edges(ind1, 2) == new_edge(2)),1) == 0) || (size(find(edges(ind1, 2) == new_edge(2)),2) == 0)) && ((size(find(edges(ind2, 1) == new_edge(2)),1) == 0) || (size(find(edges(ind2, 1) == new_edge(2)),2) == 0))
+            if ((size(find(edges(ind1, 2) == new_edge(2)),1) == 0) || (size(find(edges(ind1, 2) == new_edge(2)),2) == 0)) && ...
+                    ((size(find(edges(ind2, 1) == new_edge(2)),1) == 0) || (size(find(edges(ind2, 1) == new_edge(2)),2) == 0)) ...
+                    && (isempty(find(new_edge == 0, 1)))
                 counter = counter+1;
                 edges(counter,:) = new_edge;
             end     
         end
     end
 end
-
+n
 verts_children = zeros(N, 7);%[]; %zeros(N, 4);
 verts_children_faces = zeros(N, 7);
 edges_children = zeros(size(edges,1), 4);
 %verts_children = [];
 %edges_children = [];
+counter = 1;
 %Loop through faces
 for j=1:size(faces,1)
     
@@ -59,9 +63,13 @@ for j=1:size(faces,1)
     F = [mean(X); mean(Y); mean(Z)];
     
     %loop through vertices of the current face and create their children 
-    vertices_refined(n*j - n + 1:n*j, 1) = (1 - alpha).*X + alpha.*F(1).*ones(1, length(X))';
-    vertices_refined(n*j - n + 1:n*j, 2) = (1 - alpha).*Y + alpha.*F(2).*ones(1, length(X))';
-    vertices_refined(n*j - n + 1:n*j, 3) = (1 - alpha).*Z + alpha.*F(3).*ones(1, length(X))';
+%     vertices_refined(n_local*j - n_local + 1:n_local*j, 1) = (1 - alpha).*X + alpha.*F(1).*ones(1, length(X))';
+%     vertices_refined(n_local*j - n_local + 1:n_local*j, 2) = (1 - alpha).*Y + alpha.*F(2).*ones(1, length(X))';
+%     vertices_refined(n_local*j - n_local + 1:n_local*j, 3) = (1 - alpha).*Z + alpha.*F(3).*ones(1, length(X
+    
+    vertices_refined(counter:counter+n_local-1, 1) = (1 - alpha).*X + alpha.*F(1).*ones(1, length(X))';
+    vertices_refined(counter:counter+n_local-1, 2) = (1 - alpha).*Y + alpha.*F(2).*ones(1, length(X))';
+    vertices_refined(counter:counter+n_local-1, 3) = (1 - alpha).*Z + alpha.*F(3).*ones(1, length(X))';
     
     for k = 0:n_local-1
 
@@ -71,7 +79,7 @@ for j=1:size(faces,1)
                 ind = ind+1;
             end
         end
-        verts_children(faces(j, k+1), ind) = n*j-n+k+1;
+        verts_children(faces(j, k+1), ind) = counter+k;
         verts_children_faces(faces(j, k+1), ind) = j;
     end
     for k = 0:n_local-1
@@ -87,26 +95,24 @@ for j=1:size(faces,1)
         end
         
         ind = 1;
-%         if (current_edge_id == 376)
-%             temp = temp + 1
-%             weird_edges = [weird_edges j]
-%         end
+      %  current_edge_id
         while (edges_children(current_edge_id, ind) ~= 0 )
             ind = ind+1;
         end
-        edges_children(current_edge_id, ind) = n*j-n + k + 1;
-        edges_children(current_edge_id, ind+1) = n*j -n + mod(k+1,n) + 1;
+        edges_children(current_edge_id, ind) = counter + k;
+        edges_children(current_edge_id, ind+1) = counter + mod(k+1,n_local);
         
     end
     
     %add the face formed by children vertices of the old face j
-    faces_refined(j, 1:n_local) = n*j-n+1:n*j;
+    faces_refined(j, 1:n_local) = counter:counter + n_local - 1;
+    counter = counter + n_local;
     colours(j) = 1;
 end
 %verts_children
 
 %loop through old vertices
-counter = size(faces,1);
+counter = size(faces_refined,1);
 for i = 1:N
 
     new_face = verts_children(i, (1:nnz(verts_children(i, :))));
@@ -117,12 +123,12 @@ for i = 1:N
         k = 1;
         for iter = 1:length(new_face)-1
 
-            for j = 2:length(neighbouring_faces)
+            for j = 1:length(neighbouring_faces)
                 %bad smell. this check is false when we compare the face to
                 %itsel. In the final version there shouldn't be any
                 %comparissons with itself at all, of course
               
-                if (length(intersect(faces(neighbouring_faces(k),:),faces(neighbouring_faces(j),:))) == 2) && (isempty(find(face_ordered == new_face(j))))
+                if (length(intersect(faces(neighbouring_faces(k),1:nnz(faces(neighbouring_faces(k),:))),faces(neighbouring_faces(j),1:nnz(faces(neighbouring_faces(j),:))))) == 2) && (isempty(find(face_ordered == new_face(j))))
                     face_ordered = [face_ordered, new_face(j)];
                     k = j;
                 end
