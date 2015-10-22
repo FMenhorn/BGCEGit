aa0706cfe0f80f0cb33b3deb59dfed3210e097f3
EWannerberg <daplff@gmail.com>
2015-10-22 13:35:13 +0200

    extra docu an stuff

--- Prototypes/MATLAB/Sandbox/GSpline/createCoefsMatrix.m
+++ Prototypes/MATLAB/Sandbox/GSpline/createCoefsMatrix.m
@@ -3,9 +3,10 @@ function coefsMatrix = createCoefsMatrix(parameterCoordinates)
 domainM = 14;
 domainN = 8;
 
-% takes parameterCoordinates[Nx2](on a patch that spans over [0,1] with 4x4
+% takes parameterCoordinates[Nx2](on a patch that spans over [0,1] with
+% domainMxdomainN
 % local bezier patches) and computes the coefficient matrix for each of
-% these as a Nx6x6 matrix (where the first and last columns/rows are the
+% these as a Nx (domainM +2) x (domainN + 2) matrix (where the first and last columns/rows are the
 % neighbouring points)
 N = size(parameterCoordinates,1);
 coefsMatrix = zeros(N,domainM+2,domainN+2);
