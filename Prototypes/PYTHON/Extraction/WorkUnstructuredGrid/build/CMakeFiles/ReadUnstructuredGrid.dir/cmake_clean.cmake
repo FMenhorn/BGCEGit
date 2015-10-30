82fcc491ea9d948e76c56ddc8f73a24b98ac4550
JuanCarlosCSE <medinas.juancarlos@gmail.com>
2015-10-30 06:40:05 -0600

    Adding extraction

---
+++ Prototypes/PYTHON/Extraction/WorkUnstructuredGrid/build/CMakeFiles/ReadUnstructuredGrid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ReadUnstructuredGrid.dir/ReadUnstructuredGrid.cxx.o"
+  "ReadUnstructuredGrid.pdb"
+  "ReadUnstructuredGrid"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ReadUnstructuredGrid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
