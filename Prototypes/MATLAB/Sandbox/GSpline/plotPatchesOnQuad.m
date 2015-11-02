651bf20f8877e92325114a0980a4ebf80f6ceead
EWannerberg <daplff@gmail.com>
2015-11-02 23:49:11 +0100

    added function to plot bezier patches from quad patch points/doo-sabin-refined vertices/vertex control points/whatever you call them! Uncomment PlotPoints in plotPatchesOnQuad and plotOneQuadWhole to get the bezier control points. Play around and enjoy!

---
+++ Prototypes/MATLAB/Sandbox/GSpline/plotPatchesOnQuad.m
@@ -0,0 +1,64 @@
+function plotHandle = plotPatchesOnQuad(quad_list,AVertexList,B1VertexList,B2VertexList,CVertexList,quad_control_point_indices,control_points,quad_index,patch_list,plotHandle)
+
+whichCornerList = [1,4;2,3];
+
+one4toone2 = @(i) (i-1)/3 + 1;
+
+whichCornerFun = @(i,j) whichCornerList(one4toone2(i),one4toone2(j));
+
+controlVertices = zeros(3,3,3);
+
+mod_index = @(index,modul) mod(index-1,modul) + 1;
+indexToI = @(index) mod_index(index,4);
+indexToJ = @(index) (index - indexToI(index))/4 + 1;
+
+figure(plotHandle);
+% = figure('Name', 'Bezier surface plot');
+hold on;
+q = quad_index;
+
+for index = patch_list
+    j = indexToJ(index);
+    i = indexToI(index);
+    
+    if ([i,j]==1 | [i,j] == 4)
+        whichCorner = whichCornerFun(i,j);
+        indexMask = getExtraOrdCornerIndexMask(quad_list,AVertexList,B1VertexList,B2VertexList,CVertexList,quad_control_point_indices,q,whichCorner);
+        numberOfEdges = size(indexMask,2);
+        As =  control_points(indexMask(1,:),:)';
+        B1s =  control_points(indexMask(2,:),:)';
+        B2s =  control_points(indexMask(3,:),:)';
+        Cs =  control_points(indexMask(4,:),:)';
+        Bs = cat(2,reshape(B1s,3,1,numberOfEdges),reshape(B2s,3,1,numberOfEdges));
+        patch = getBicubicPatchIndex(1,As,Bs,Cs);
+        %     patch(:,3,3) = rot(pi)*patch(:,3,3);
+%         plotPoints(patch);
+        [xx,yy,zz]=bezier(patch,0.1);
+        surf(xx,yy,zz)
+        
+        
+    else
+        neighbourMask = get3x3ControlPointIndexMask(quad_list,quad_control_point_indices,q,[i,j]);
+        for bezierJ = 1:3
+            for bezierI = 1:3
+                controlVertices(:,bezierI,bezierJ) = control_points(neighbourMask(bezierI,bezierJ),:);
+            end
+        end
+        
+        APatch = getBiquadraticPatch(controlVertices);
+        %     patch(:,3,3) = rot(pi)*patch(:,3,3);
+%         plotPoints(APatch);
+        [xx,yy,zz]=bezier(APatch,0.1);
+        surf(xx,yy,zz)
+        
+        
+        
+    end
+end
+
+end
+
+
+
+
+
--- Prototypes/MATLAB/Sandbox/GSpline/plotQuadOrQuadsWithHairs.m
+++ Prototypes/MATLAB/Sandbox/GSpline/plotQuadOrQuadsWithHairs.m
@@ -3,21 +3,38 @@ clear all
 close all
 quadsToPlot = 1:3;
 
+
 %create data
 createTorusParams;
 loadingTorus;
 coefs = createGlobalControlMeshCoefs(parameters,quads_Torus,newA,newB1,newB2,newC,regularPoints);
 vertices = coefs\torus_verts_fine; 
+numQuads = size(regularPoints,1);
+
 
 
 %plot
 fig_handle = figure('Name', 'Plot with hairs');
+
+quads_patchvertices = zeros(3,numQuads,16);
+for i = 1:numQuads
+    temp_patchvertices = getPatchPointsOnQuad(quads_Torus,createdQuadVerts,i,fig_handle);
+    for j = 1:4
+        for k = 1:4
+            quads_patchvertices(:,i,k + (j-1)*4) = temp_patchvertices(:,k,j);
+        end
+    end
+end
+
+regular_quadpoints = shiftToRegularPointsFormat(regularPoints,quads_patchvertices);
 axis off
-for i = quadsToPlot
+for i = 1:length(quadsToPlot)
     %plotAllHairsOnQuad(quads_Torus,createdQuadVerts,createdPoints,parameters,i,fig_handle);
-    %plotOneQuadWhole(quads_Torus,newA,newB1,newB2,newC,regularPoints,vertices,i,fig_handle);
-    plotLinesOnQuad(quads_Torus,createdQuadVerts,i,fig_handle);
-    plotPatchPointsOnQuad(quads_Torus,createdQuadVerts,i,fig_handle);
+%     plotOneQuadWhole(quads_Torus,newA,newB1,newB2,newC,regularPoints,vertices,i,fig_handle);
+    plotLinesOnQuad(quads_Torus,createdQuadVerts,quadsToPlot(i),fig_handle);
+    plotPatchPointsOnQuad(quads_Torus,createdQuadVerts,quadsToPlot(i),fig_handle);
+    plotPatchesOnQuad(quads_Torus,newA,newB1,newB2,newC,regularPoints,regular_quadpoints,i,[5,8,9,12],fig_handle);
+    
     
     %------------------------
     %ADDED FOR ANNA'S PURPOSES
@@ -28,3 +45,4 @@ for i = quadsToPlot
     %plotPoints(bezier_points);
     %-------------------------
 end
+
