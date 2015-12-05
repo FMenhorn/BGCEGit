88f0aecf9a4e7dac95694882d287e92286002a1c
EWannerberg <daplff@gmail.com>
2015-12-05 11:14:37 +0100

    Bez-point creating function made to output matrices with all points and then matrices with what indices belong together

--- Prototypes/MATLAB/Sandbox/GSpline/createBezierPointMatrices.m
+++ Prototypes/MATLAB/Sandbox/GSpline/createBezierPointMatrices.m
@@ -1,18 +1,22 @@
-function [biquadtraticMatrix, bicubicMatrix] = createBezierPointMatrices(quad_list,AVertexList,B1VertexList,B2VertexList,CVertexList,quad_control_point_indices,control_points)
+function [biquadtraticMatrix,biquadraticIndices, bicubicMatrix, bicubicIndices] = createBezierPointMatrices(quad_list,AVertexList,B1VertexList,B2VertexList,CVertexList,quad_control_point_indices,control_points)
 
 whichCornerList = [1,4;2,3];
 
 one4toone2 = @(i) (i-1)/3 + 1;
 
 whichCornerFun = @(i,j) whichCornerList(one4toone2(i),one4toone2(j));
-
-controlVertices = zeros(3,3,3);
+% 
+% controlVertices = zeros(3,3,3);
 
 number_of_quads = size(quad_list,1);
 counterBic = 1;
 counterBiq = 1;
-biquadtraticMatrix = zeros(12*number_of_quads,9,3);
-bicubicMatrix = zeros(4*number_of_quads,16,3);
+biquadtraticMatrix = zeros(12*9*number_of_quads,3);
+biquadraticIndices = zeros(12*number_of_quads,9);
+bicubicMatrix = zeros(4*16*number_of_quads,3);
+bicubicIndices = zeros(4*number_of_quads,16);
+
+getLinearIndexing = @(i,j,width) i + (j-1)*width;
 hold on;
 for q = 1:number_of_quads
     for j = 1:4
@@ -29,25 +33,23 @@ for q = 1:number_of_quads
                 Bs = cat(2,reshape(B1s,3,1,numberOfEdges),reshape(B2s,3,1,numberOfEdges));
                 patch = getBicubicPatchIndex(1,As,Bs,Cs);
                 %     patch(:,3,3) = rot(pi)*patch(:,3,3);
-                for iPatch = 1:4
-                    for jPatch = 1:4
+                for jPatch = 1:4
+                    for iPatch = 1:4
+                        currentIndex = (counterBic -1)*16 + getLinearIndexing(iPatch,jPatch,4);
                         
-                        bicubicMatrix(counterBic,(iPatch+(jPatch-1)*3),:) = patch(:,iPatch,jPatch);
+                        bicubicMatrix(currentIndex,:) = patch(:,iPatch,jPatch);
+                        bicubicIndices(counterBic,getLinearIndexing(iPatch,jPatch,4)) = currentIndex;
                     end
                 end
                 counterBic = counterBic + 1;
-                
             else
                 neighbourMask = get3x3ControlPointIndexMask(quad_list,quad_control_point_indices,q,[i,j]);
-                for bezierJ = 1:3
-                    for bezierI = 1:3
-                        controlVertices(:,bezierI,bezierJ) = control_points(neighbourMask(bezierI,bezierJ),:);
-                    end
-                end
-                for iPatch = 1:3
-                    for jPatch = 1:3
+                for jPatch = 1:3
+                    for iPatch = 1:3
+                        currentIndex = (counterBiq -1)*9 + getLinearIndexing(iPatch,jPatch,3);
                         
-                        biquadtraticMatrix(counterBiq,(iPatch+(jPatch-1)*3),:) = controlVertices(:,iPatch,jPatch);
+                        biquadtraticMatrix(currentIndex,:) = control_points(neighbourMask(iPatch,jPatch),:);
+                        biquadraticIndices(counterBiq,getLinearIndexing(iPatch,jPatch,3)) = currentIndex;
                     end
                 end
                 %     patch(:,3,3) = rot(pi)*patch(:,3,3);
