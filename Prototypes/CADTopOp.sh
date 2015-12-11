76a4a2d84ad69c83ff3d0d41b63a9fe5c6d4b0a4
Friedrich Menhorn <menhorn@in.tum.de>
2015-12-11 17:12:12 +0100

    adapted CADTopOp script to recent folder structure changes

--- Prototypes/CADTopOp.sh
+++ Prototypes/CADTopOp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cd OpenCascade
+cd OpenCascade/Code
 ./build/CADToVoxel $1 $2 $3 $4
 cd ..
-python OpenCascade/optimise.py OpenCascade/topy_${2}.tpd
+cd ..
+python OpenCascade/Code/optimise.py OpenCascade/Code/topy_${2}.tpd
 cp topy_${2}_099.vtk PYTHON/Extraction/WorkUnstructuredGrid/
 cd PYTHON/Extraction/WorkUnstructuredGrid 
 python Ugrid_to_Python.py topy_${2}_099.vtk
