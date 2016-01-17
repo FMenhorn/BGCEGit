c4aee9c4dc9aed8209da05b3733d876609277c0c
Benjamin Rüth <benjamin.rueth@tum.de>
2016-01-17 13:03:27 +0100

    some docu and nicer code structure

--- Prototypes/PYTHON/Sandbox/Back2CAD/NURBSToSTEP.py
+++ Prototypes/PYTHON/Sandbox/Back2CAD/NURBSToSTEP.py
@@ -7,30 +7,34 @@ import Part
 import Import
 import numpy as np
 
+"""
+configuration parameters:
 
-def sample_patch(N_nodes):
-    x0 = 1.0
-    y0 = 1.0
-    z0 = 0.0
-    h = .1
-    nurbs_pts = N_nodes * N_nodes * [None]
-    patch = []
+for the length of the knot vector, the following rule has to hold:
+n_knots = n_nodes + deg + 1
+n_nodes has to be chosen such that it matches with the given number of control nodes per patch in the input files
+"""
+input_folder = "./TorusFairNURBS"
+output_file_name = "./FairTorusNURBS.step"
+plot_control_points = False
 
-    id = 0
-    for i in range(N_nodes):
-        for j in range(N_nodes):
-            x = x0 + i * h
-            y = y0 + j * h
-            z = x0 + i**2 * h
-            nurbs_pts[i+N_nodes*j]=[x,y,z]
-            id = id + 1
-            patch.append(id)
-
-    return [patch], nurbs_pts
+knots = [0, 0, 0, 0, .25, 0.25, 0.5, 0.5, 0.5, 0.75, .75, 1, 1, 1, 1]
+degree = 3
+n_nodes = 11
+assert (knots.__len__() == n_nodes + degree + 1)
+"""
+configuration done
+"""
 
 
 def parse_csv_into_matrix(csv_path, out_type):
-    print "parsing file at "+csv_path
+    """
+    parses a csv file into a list(list(out_type))
+    :param csv_path: path to the file to be parsed
+    :param out_type: defines the expected type. e.g. int or float
+    :return: list(list(out_type)) holding content of csv
+    """
+    print "Parsing file at " + csv_path + "..."
     matrix = []
     with open(csv_path) as csvfile:
         idxreader = csv.reader(csvfile, delimiter=',')
@@ -39,49 +43,49 @@ def parse_csv_into_matrix(csv_path, out_type):
             for idx in row:
                 new_row.append(out_type(idx))
             matrix.append(new_row)
+    print "File parsed."
 
     return matrix
 
 
 def parse_all_from_folder(folder_path):
-
-    nurbs_idx = parse_csv_into_matrix(folder_path+'/NURBSPatchIndices.csv',int)
-    nurbs_pts = parse_csv_into_matrix(folder_path+'/NURBSPatchPoints.csv',float)
-
+    """
+    Parses a list holding all vertices and a list holding the indices of the vertices contributing to each patch from
+    .csv input files. The input folder is defined above in the configuration.
+    :param folder_path: path to folder holding the relevant csv fildes.
+    :return: two list of vertices and vertex ids respectively
+    """
+    nurbs_idx = parse_csv_into_matrix(folder_path + '/NURBSPatchIndices.csv', int)
+    nurbs_pts = parse_csv_into_matrix(folder_path + '/NURBSPatchPoints.csv', float)
     return nurbs_idx, nurbs_pts
 
 
 def get_vertices(patch_ids, vertex_list):
+    """
+    Extracts the relevant vertices for a single patch from the vertex list.
+    :param patch_ids: list of vertex ids of current patch
+    :param vertex_list: list of all vertices
+    :return: list of vertices of the current patch in lexicographical order
+    """
     vertices = []
     for v_id in patch_ids:
-        v_id = v_id - 1 # MATLAB indexing -> PYTHON indexing!!!
+        v_id = v_id - 1  # MATLAB indexing -> PYTHON indexing!!!
         vertices.append(vertex_list[v_id])
     return vertices
 
 
-def generate_bspline_patch(vertices, n_nodes_u, n_nodes_v):
-    def knot_uniform(deg, n_nodes):
-        n_knots = n_nodes + deg + 1
-        knots = np.zeros(n_knots)
-
-        for i in range(deg):
-            knots[i] = 0
-        for i in range(n_nodes):
-            knots[i+deg] = (i) / (n_nodes-deg)
-        for i in range(deg):
-            knots[-i-1] = 1
-
-        return knots.tolist()
-
-    degree_u = 1
-    degree_v = 1
-    #knot_u = [0, 0, 0, 0, .25, 0.25, 0.25, 0.5, 0.5, .5, 0.75, 0.75, .75, 1, 1, 1, 1]
-    #knot_v = [0, 0, 0, 0, .25, 0.25, 0.25, 0.5, 0.5, .5, 0.75, 0.75, .75, 1, 1, 1, 1]
-    knot_u = knot_uniform(degree_u, n_nodes_u)
-    knot_v = knot_uniform(degree_v, n_nodes_v)
-    #[0,0,0,0,0.25,0.25,0.25,0.5,0.5,0.75,0.75,0.75,1,1,1,1]
-    #knot_u=[0,0,0,.5,1,1,1]
-    #knot_v=[0,0,0,.5,1,1,1]
+def generate_bspline_patch(vertices):
+    """
+    Generates a bspine patch from the given vertices. Parameters like degree of the patch, knot vector and number of
+    control points are defined above.
+    :param vertices: lexicographically numbered control points in a 2D Array of 3 component points
+    """
+    n_nodes_u = n_nodes
+    n_nodes_v = n_nodes
+    degree_u = degree
+    degree_v = degree
+    knot_u = knots
+    knot_v = knots
 
     patch = Part.BSplineSurface()
     patch.increaseDegree(degree_u, degree_v)
@@ -95,39 +99,33 @@ def generate_bspline_patch(vertices, n_nodes_u, n_nodes_v):
 
     for ii in range(0, n_nodes_u):
         for jj in range(0, n_nodes_v):
-            k = ii+jj*n_nodes_u
+            k = ii + jj * n_nodes_u
             v = vertices[k]
-            control_point = FreeCAD.Vector(v[0],v[1],v[2])
-            patch.setPole(ii+1,jj+1,control_point, 1)
-            #Part.show(Part.Vertex(control_point))
+            control_point = FreeCAD.Vector(v[0], v[1], v[2])
+            patch.setPole(ii + 1, jj + 1, control_point, 1)
+            if(plot_control_points):
+                Part.show(Part.Vertex(control_point))  # plotting corresponding control points, switched on/off in configuration section
+    Part.show(patch.toShape())  # plots the patch
 
-    Part.show(patch.toShape())
 
-
+print "Creating FreeCAD Document..."
 doc = FreeCAD.newDocument("tmp")
-print "Document created"
+print "FreeCAD Document created."
 
-nurbs_idx, nurbs_pts = parse_all_from_folder('./TorusFairNURBS')
-print "Stuff parsed"
+print "Parsing input..."
+nurbs_idx, nurbs_pts = parse_all_from_folder(input_folder)
+print "Input files from " + input_folder + " parsed."
 
-N_nodes = 11
-
-# uncomment for plotting sample patch
-#nurbs_idx, nurbs_pts = sample_patch(N_nodes)
-#
-
-# uncomment for plotting only one of many patches
-#nurbs_idx = [nurbs_idx[0]]
-#
-
+print "Plotting patches..."
+patch_id = 0
 for patch in nurbs_idx:
+    print "Plotting patch no. " + str(patch_id) + "..."
     vertices = get_vertices(patch, nurbs_pts)
-    generate_bspline_patch(vertices, N_nodes, N_nodes)
+    generate_bspline_patch(vertices)
+    patch_id += 1
+print "All patches plotted."
 
+print "Exporting file..."
 __objs__ = FreeCAD.getDocument("tmp").findObjects()
-
-Import.export(__objs__, "./FairTorusNURBS.step")
-print ".step exported"
-
-
-
+Import.export(__objs__, output_file_name)
+print "Output file " + output_file_name + " exported."
