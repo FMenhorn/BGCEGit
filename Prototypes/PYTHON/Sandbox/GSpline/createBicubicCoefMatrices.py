7c528125bd51775bfcc34da42ef2c69841b23e2b
Benjamin Rüth <benjamin.rueth@tum.de>
2016-01-31 20:47:51 +0100

    fixed and validated function

--- Prototypes/PYTHON/Sandbox/GSpline/createBicubicCoefMatrices.py
+++ Prototypes/PYTHON/Sandbox/GSpline/createBicubicCoefMatrices.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import numpy as np
 import math
 
@@ -30,72 +32,76 @@ def createBicubicCoefMatrices(num_of_quads):
     Ccoefs[0][0][0] = 0.25
     Acoefs[0][0][0] = 0.25
 
-    B1coefs[0][1][0] = 1.0 / 12
-    B2coefs[0][1][0] = 5.0 / 12
-    Ccoefs[0][1][0] = 5.0 / 12
-    Acoefs[0][1][0] = 1.0 / 12
+    B1coefs[0][1][0] = 1.0 / 12.0
+    B2coefs[0][1][0] = 5.0 / 12.0
+    Ccoefs[0][1][0] = 5.0 / 12.0
+    Acoefs[0][1][0] = 1.0 / 12.0
 
-    B2coefs[1][2][0] = 5.0 / 12
-    B1coefs[0][2][0] = 1.0 / 12
-    Ccoefs[0][2][0] = 5.0 / 12
-    Ccoefs[1][2][0] = 1.0 / 12
+    B2coefs[0][2][0] = 5.0 / 12.0
+    B1coefs[1][2][0] = 1.0 / 12.0
+    Ccoefs[0][2][0] = 5.0 / 12.0
+    Ccoefs[1][2][0] = 1.0 / 12.0
 
     B2coefs[0][3][0] = 0.25
     B1coefs[1][3][0] = 0.25
     Ccoefs[0][3][0] = 0.25
     Ccoefs[1][3][0] = 0.25
 
-    B2coefs[0][1][1] = 5 / 36
-    B1coefs[0][1][1] = 5 / 36
-    Ccoefs[0][1][1] = (25 + 4 * a) / 36
-    Acoefs[0][1][1] = (1 - 4 * a) / 36
+    B2coefs[0][1][1] = 5.0 / 36.0
+    B1coefs[0][1][1] = 5.0 / 36.0
+    Ccoefs[0][1][1] = (25.0 + 4.0 * a) / 36.0
+    Acoefs[0][1][1] = (1.0 - 4.0 * a) / 36.0
 
-    B2coefs[0][2][1] = (5 - 10 * a) / 36
-    B1coefs[1][2][1] = (1 + 2 * a) / 36
-    Ccoefs[0][2][1] = (25 + 6 * a) / 36
-    Ccoefs[1][2][1] = (5 + 2 * a) / 36
+    B2coefs[0][2][1] = (5.0 - 10.0 * a) / 36.0
+    B1coefs[1][2][1] = (1.0 + 2.0 * a) / 36.0
+    Ccoefs[0][2][1] = (25.0 + 6.0 * a) / 36.0
+    Ccoefs[1][2][1] = (5.0 + 2.0 * a) / 36.0
 
-    for i in range(0, num_of_quads - 1):
-        Ccoefs[i][3][3] = 1 / num_of_quads
+    for i in range(0, num_of_quads):
+        Ccoefs[i][3][3] = 1.0 / num_of_quads
 
-    B2coefs[0][3][1] = (1 - 2 * a) / 12
-    B1coefs[1][3][1] = (1 - 2 * a) / 12
-    Ccoefs[0][3][1] = (5 + 2 * a) / 12
-    Ccoefs[1][3][1] = (5 + 2 * a) / 12
+    B2coefs[0][3][1] = (1.0 - 2.0 * a) / 12.0
+    B1coefs[1][3][1] = (1.0 - 2.0 * a) / 12.0
+    Ccoefs[0][3][1] = (5.0 + 2.0 * a) / 12.0
+    Ccoefs[1][3][1] = (5.0 + 2.0 * a) / 12.0
 
-    for i in range(0, num_of_quads - 1):
-        Ccoefs[i][3][2] += 1 / num_of_quads
+    for i in range(0, num_of_quads):
+        Ccoefs[i][3][2] += 1.0 / num_of_quads
 
-    for l in range(0, num_of_quads - 1):
-        Ccoefs[l][3][2] += (2 * a / (3 * c * num_of_quads)) * (np.cos(2 * np.pi * (l - 1) / num_of_quads) +
-                                                               np.cos(2 * np.pi * (l % num_of_quads) / num_of_quads))
+    for l in range(0, num_of_quads):
+        Ccoefs[l][3][2] += (2.0 * a / (3.0 * c * num_of_quads)) * (np.cos(2.0 * np.pi * (l - 1) / num_of_quads) +
+                                                               np.cos(2.0 * np.pi * (l % num_of_quads) / num_of_quads))
     shiftReversed = shiftReverse(0, num_of_quads)
 
-    for j in range(0, 2):
-        for i in range(j, 3):
+    for j in range(0, 3):
+        for i in range(j+1, 4):
             i_symm = j
             j_symm = i
 
-            for k in range(0, num_of_quads - 1):
+            for k in range(0, num_of_quads):
                 Acoefs[k][i_symm][j_symm] = Acoefs[shiftReversed[k]][i][j]
                 B1coefs[k][i_symm][j_symm] = B2coefs[shiftReversed[k]][i][j]
                 B2coefs[k][i_symm][j_symm] = B1coefs[shiftReversed[k]][i][j]
                 Ccoefs[k][i_symm][j_symm] = Ccoefs[shiftReversed[k]][i][j]
 
     if np.mod(num_of_quads, 2) == 1:
-        for i in range(1, num_of_quads):
-            h_three_coefs_result = h_three_coefs(i, Ccoefs, c, num_of_quads)
-            for j in range(0, num_of_quads - 1):
-                Ccoefs[k][2][2] -= ((-1) ** i) * h_three_coefs_result[j]
-                B1coefs[k][2][2] -= ((-1) ** i) * h_three_coefs_result[j]
-                B2coefs[k][2][2] -= ((-1) ** i) * h_three_coefs_result[j]
+        for i in range(1, num_of_quads+1):
+            Ccoefs_h_three_coefs_result = h_three_coefs(i, Ccoefs, c, num_of_quads)
+            B1coefs_h_three_coefs_result = h_three_coefs(i, B1coefs, c, num_of_quads)
+            B2coefs_h_three_coefs_result = h_three_coefs(i, B2coefs, c, num_of_quads)
+            for j in range(0, num_of_quads):
+                Ccoefs[j][2][2] -= ((-1) ** i) * Ccoefs_h_three_coefs_result[j]
+                B1coefs[j][2][2] -= ((-1) ** i) * B1coefs_h_three_coefs_result[j]
+                B2coefs[j][2][2] -= ((-1) ** i) * B2coefs_h_three_coefs_result[j]
     else:
-        for i in range(1, num_of_quads):
-            h_three_coefs_result = h_three_coefs(i, Ccoefs, c, num_of_quads)
-            for j in range(0, num_of_quads - 1):
-                Ccoefs[k][2][2] -= ((-1) ** i) * (num_of_quads - i) * h_three_coefs_result[j] * 2 / num_of_quads
-                B1coefs[k][2][2] -= ((-1) ** i) * (num_of_quads - i) * h_three_coefs_result[j] * 2 / num_of_quads
-                B2coefs[k][2][2] -= ((-1) ** i) * (num_of_quads - i) * h_three_coefs_result[j] * 2 / num_of_quads
+        for i in range(1, num_of_quads+1):
+            Ccoefs_h_three_coefs_result = h_three_coefs(i, Ccoefs, c, num_of_quads)
+            B1coefs_h_three_coefs_result = h_three_coefs(i, B1coefs, c, num_of_quads)
+            B2coefs_h_three_coefs_result = h_three_coefs(i, B2coefs, c, num_of_quads)
+            for j in range(0, num_of_quads):
+                Ccoefs[j][2][2] -= ((-1) ** i) * (num_of_quads - i) * Ccoefs_h_three_coefs_result[j] * 2.0 / num_of_quads
+                B1coefs[j][2][2] -= ((-1) ** i) * (num_of_quads - i) * B1coefs_h_three_coefs_result[j] * 2.0 / num_of_quads
+                B2coefs[j][2][2] -= ((-1) ** i) * (num_of_quads - i) * B2coefs_h_three_coefs_result[j] * 2.0 / num_of_quads
 
     # return {'Acoefs': Acoefs, 'B1coefs': B1coefs, 'B2coefs': B2coefs, 'Ccoefs': Ccoefs}
     return [Acoefs, B1coefs, B2coefs, Ccoefs]
@@ -121,6 +127,6 @@ def h_three_coefs(ind, whichArray, c, num_of_quads):
     shiftedIndices = shifted_indices(ind, num_of_quads)
     ans = [0 for i in range(0, len(shiftedIndices))]
     for i in range(0, len(shiftedIndices)):
-        ans[i] = (1 - 2 * c / 3) * whichArray[shiftedIndices[i]][3][2] + (2 * c / 3) * whichArray[
+        ans[i] = (1.0 - 2.0 * c / 3.0) * whichArray[shiftedIndices[i]][3][2] + (2.0 * c / 3.0) * whichArray[
             shiftedIndices[i]][3][1]
     return ans
