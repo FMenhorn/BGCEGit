adae3783e5f5f96c980be3bd72c97816ef522f51
Benjamin Rüth <benjamin.rueth@tum.de>
2016-02-26 12:32:27 +0100

    debugged

--- Prototypes/PYTHON/NURBSReconstruction/BackToCAD/NURBSToSTEPAllraised.py
+++ Prototypes/PYTHON/NURBSReconstruction/BackToCAD/NURBSToSTEPAllraised.py
@@ -1,58 +1,15 @@
 from __future__ import division
 
-import csv
+import sys
+
+FREECADPATH = '/usr/lib/freecad/lib'
+sys.path.append(FREECADPATH)
 
 import FreeCAD
 import Part
 import Import
 
-
-"""
-configuration parameters:
-
-for the length of the knot vector, the following rule has to hold:
-n_knots = n_nodes + deg + 1
-n_nodes has to be chosen such that it matches with the given number of control nodes per patch in the input files
-"""
-#input_folder = "./Torus_Fair_NURBS_AllRaised"
-#output_file_name = "./Torus_NURBS_AllRaised.step"
-#nonchanging_file_name = "./Cone.step"
-
-"""
-configuration done
-"""
-
-def parse_csv_into_matrix(csv_path, out_type):
-    """
-    parses a csv file into a list(list(out_type))
-    :param csv_path: path to the file to be parsed
-    :param out_type: defines the expected type. e.g. int or float
-    :return: list(list(out_type)) holding content of csv
-    """
-    print "Parsing file at " + csv_path + "..."
-    matrix = []
-    with open(csv_path) as csvfile:
-        idxreader = csv.reader(csvfile, delimiter=',')
-        for row in idxreader:
-            new_row = []
-            for idx in row:
-                new_row.append(out_type(idx))
-            matrix.append(new_row)
-    print "File parsed."
-
-    return matrix
-
-
-def parse_all_from_folder(folder_path):
-    """
-    Parses a list holding all vertices and a list holding the indices of the vertices contributing to each patch from
-    .csv input files. The input folder is defined above in the configuration.
-    :param folder_path: path to folder holding the relevant csv fildes.
-    :return: two list of vertices and vertex ids respectively
-    """
-    nurbs_idx = parse_csv_into_matrix(folder_path + '/NURBSPatchIndices.csv', int)
-    nurbs_pts = parse_csv_into_matrix(folder_path + '/NURBSPatchPoints.csv', float)
-    return nurbs_idx, nurbs_pts
+PLOT_CONTROL_POINTS = False
 
 
 def get_vertices(patch_ids, vertex_list):
@@ -95,15 +52,13 @@ def generate_bspline_patch(vertices, n_nodes, degree, knots):
             v = vertices[k]
             control_point = FreeCAD.Vector(v[0], v[1], v[2])
             patch.setPole(ii + 1, jj + 1, control_point, 1)
-            #if(plot_control_points):
-            #    Part.show(Part.Vertex(control_point))  # plotting corresponding control points, switched on/off in configuration section
+            if(PLOT_CONTROL_POINTS):
+                Part.show(Part.Vertex(control_point))  # plotting corresponding control points, switched on/off in configuration section
 
     return patch.toShape()
 
 
 def export_step(nurbs_idx, nurbs_pts, output_file_name, nonchanging_file_name):
-
-    plot_control_points = False
 
     knots = [0, 0, 0, 0, 0.25, 0.25, 0.25, 0.5, 0.5, 0.5, 0.75, 0.75, 0.75, 1, 1, 1, 1]
     degree = 3
@@ -128,9 +83,9 @@ def export_step(nurbs_idx, nurbs_pts, output_file_name, nonchanging_file_name):
     patch_id = 0
     for patch in nurbs_idx:
         print "Plotting patch no. " + str(patch_id) + "..."
-        patch[:] = [x - 1 for x in patch]
         vertices = get_vertices(patch, nurbs_pts)
-        faceHolder.append(generate_bspline_patch(vertices, n_nodes, degree, knots)) # add to the list of Faces the patch converted to Shape
+        new_patch = generate_bspline_patch(vertices, n_nodes, degree, knots)
+        faceHolder.append(new_patch) # add to the list of Faces the patch converted to Shape
         patch_id += 1
     print "All patches plotted."
 
@@ -150,7 +105,6 @@ def export_step(nurbs_idx, nurbs_pts, output_file_name, nonchanging_file_name):
         Import.insert(nonchanging_file_name, "tmp")
 
         # get objects
-        __objs__ = []
         __objs__ = FreeCAD.getDocument("tmp").findObjects()
 
         # create fusion object
