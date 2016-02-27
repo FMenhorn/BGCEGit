aefc74bfe11a1176461c15c88993640b3852f8d2
YurovaA <yurova92@gmail.com>
2016-02-27 14:20:52 +0100

    hardcoding the values for faster testing

--- Prototypes/GUI/testGui/mainwindow.cpp
+++ Prototypes/GUI/testGui/mainwindow.cpp
@@ -26,8 +26,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->RefinementEdit->setValidator(new QIntValidator(0, 10, this));
     ui->Coarsening->setValidator(new QIntValidator(0, 10000000, this));
 
-    this->hide_ErrorFields();
+    ui->IGSFileInput->setText("/home/friedrich/Documents/Studium/Master_CSE/BGCE/BGCEGit/Prototypes/OpenCascade/TestGeometry/CantileverColoredNew/CantiLeverWithLoadAtEndSmallerMovedLoad.igs");
+    ui->STEPFileInput->setText("/home/friedrich/Documents/Studium/Master_CSE/BGCE/BGCEGit/Prototypes/OpenCascade/TestGeometry/CantileverColoredNew/CantiLeverWithLoadAtEndSmallerMovedLoad.stp");
 
+    ui->BooleanFileInput->setText("/home/friedrich/Documents/Studium/Master_CSE/BGCE/BGCEGit/Prototypes/PYTHON/Back2CAD/Cone.step");
+    ui->STEPOutput->setText("/home/friedrich/Documents/Studium/Master_CSE/BGCE/BGCEGit/Prototypes/GUI/build-testGui-Desktop-Debug/testBitch.step");
+    igsFile = ui->IGSFileInput->text();
+    stpFile = ui->STEPFileInput->text();
+    booleanFile = ui->BooleanFileInput->text();
+    stepOutputFile = ui->STEPFileInput->text();
+    ui->RefinementEdit->setText("0");
+    ui->ForceEdit->setText("1");
+    ui->Coarsening->setText("2");
+    ui->FairnessWeight->setText("0.5");
+    this->hide_ErrorFields();
     //this->ui->progressBar->setMinimum(0);
     //this->ui->progressBar->setMaximum(0);
     //this->ui->progressBar->hide();
@@ -54,11 +66,11 @@ void MainWindow::on_STEPFileSelector_clicked()
 {
     QStringList fileNames;
     fileNames = QFileDialog::getOpenFileNames(this, tr("Open File"),"/path/to/file/",tr("STP File (*.stp)"));
-    if(fileNames.size()==1){
+    if (fileNames.size() == 1) {
         stpFile = fileNames.first();
         ui->STEPFileInput->setText(StringHelper::cropText(ui->STEPFileInput, stpFile));
         ui->STEPFileInput->setStyleSheet("QLabel { Color : black }");
-    }else{
+    } else {
         ui->STEPFileInput->setText("Select ONE stp input file!");
         ui->STEPFileInput->setStyleSheet("QLabel { Color : red }");
     }
@@ -68,11 +80,11 @@ void MainWindow::on_IGSFileSelector_clicked()
 {
     QStringList fileNames;
     fileNames = QFileDialog::getOpenFileNames(this, tr("Open File"),"/path/to/file/",tr("IGS File (*.igs)"));
-    if(fileNames.size()==1){
+    if (fileNames.size() == 1) {
         igsFile = fileNames.first();
         ui->IGSFileInput->setText(StringHelper::cropText(ui->IGSFileInput, igsFile));
         ui->IGSFileInput->setStyleSheet("QLabel { Color : black }");
-    }else{
+    } else {
         ui->IGSFileInput->setText("Select ONE igs input file!");
         ui->IGSFileInput->setStyleSheet("QLabel { Color : red }");
     }
@@ -91,7 +103,7 @@ void MainWindow::on_runButton_clicked()
     StringHelper::getPathAndName(stpFile, stpName, stpPath);
     StringHelper::getPathAndName(igsFile, igsName, igsPath);
 
-    if (this->checkInput(igsName, stpName)){
+    //if (this->checkInput(igsName, stpName)){
         QString forceScaling = ui->ForceEdit->text();
         QString refinementLevel = ui->RefinementEdit->text();
 
@@ -121,14 +133,17 @@ void MainWindow::on_runButton_clicked()
         std::string scriptPython = "python ./../../PYTHON/NURBSReconstruction/runningScript.py " + parameterString;
         std::cout << scriptPython << std::endl;
         system(scriptPython.c_str());
-    }
+   //}
 }
 
 bool MainWindow::checkInput(QString igsName, QString stpName){
     InputVerificator verificator;
+    QString boolName, boolPath;
+    StringHelper::getPathAndName(booleanFile, boolName, boolPath);
+
     bool flag = true;
 
-    flag = flag && verificator.isEmpty(ui->Coarsening, ui->ErrorField_coarsening, "Please enter the coarsening!");
+    flag = flag && verificator.isEmpty(ui->Coarsening, ui->ErrorField_coarsening, "Please enter the coarsening");
     flag = verificator.isEmpty(ui->FairnessWeight, ui->ErrorField_fairness, "Please enter the fairness weight") && flag;
     flag = verificator.isEmpty(ui->ForceEdit, ui->ErrorField_force, "Please enter the force") && flag;
     flag = verificator.isEmpty(ui->RefinementEdit, ui->ErrorField_refinement, "Please enter the refinement") && flag;
@@ -136,6 +151,7 @@ bool MainWindow::checkInput(QString igsName, QString stpName){
     flag = verificator.areSame(stpName, igsName, ui->STEPFileInput, ui->IGSFileInput) && flag;
     flag = verificator.checkFileName(this->stpFile, stpName, ".stp", ui->STEPFileInput) && flag;
     flag = verificator.checkFileName(this->igsFile, igsName, ".igs", ui->IGSFileInput) && flag;
+    flag = verificator.checkFileName(this->booleanFile, boolName, ".step", ui->BooleanFileInput) && flag;
     return flag;
 }
 
@@ -160,11 +176,11 @@ void MainWindow::on_BooleanFileSelector_clicked()
 {
     QStringList fileNames;
     fileNames = QFileDialog::getOpenFileNames(this, tr("Open File"),"/path/to/file/",tr("STEP File (*.step)"));
-    if(fileNames.size() == 1 && fileNames.first().size() > 0){
+    if (fileNames.size() == 1 && fileNames.first().size() > 0) {
         booleanFile = fileNames.first();
         ui->BooleanFileInput->setText(StringHelper::cropText(ui->BooleanFileInput, booleanFile));
         ui->BooleanFileInput->setStyleSheet("QLabel { Color : black }");
-    }else{
+    } else {
         booleanFile = "";
         ui->BooleanFileInput->setText("Select ONE step input file!");
         ui->BooleanFileInput->setStyleSheet("QLabel { Color : red }");
