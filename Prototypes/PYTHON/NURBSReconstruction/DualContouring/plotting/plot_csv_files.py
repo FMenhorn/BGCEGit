8c91562587026abc419f4fb5177bed893a243e55
Benjamin Rüth <benjamin.rueth@tum.de>
2016-02-29 20:16:17 +0100

    added dimensions

--- Prototypes/PYTHON/NURBSReconstruction/DualContouring/plotting/plot_csv_files.py
+++ Prototypes/PYTHON/NURBSReconstruction/DualContouring/plotting/plot_csv_files.py
@@ -17,7 +17,30 @@ ax = Axes3D(fig)
 
 DC_plotting.plot_surface(axis=ax, quads=quads, verts=verts, color='b', alpha=.5)
 
-fig.show()
+dimensions = {'xmin':-170,'xmax':195,'ymin':100,'ymax':340,'zmin':-30,'zmax':120}
+
+x_mean = (dimensions['xmax']-dimensions['xmin'])/2.0
+y_mean = (dimensions['ymax']-dimensions['ymin'])/2.0
+z_mean = (dimensions['zmax']-dimensions['zmin'])/2.0
+
+x_min = dimensions['xmin']
+y_min = dimensions['ymin']
+z_min = dimensions['zmin']
+minmin = min([x_min, y_min, z_min])
+
+x_max = dimensions['xmax']
+y_max = dimensions['ymax']
+z_max = dimensions['zmax']
+maxmax = max([x_max, y_max, z_max])
+
+width = maxmax-minmin
+
+ax.set_xlim3d(x_mean-width*.5, x_mean+width*.5)
+ax.set_ylim3d(y_mean-width*.5, y_mean+width*.5)
+ax.set_zlim3d(z_mean-width*.5, z_mean+width*.5)
+ax.set_aspect('equal')
+
+plt.show()
 
 
 
