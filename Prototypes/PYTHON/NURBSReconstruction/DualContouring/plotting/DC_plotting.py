8c67913e27c21c9e1fa16fd2e1a8976045706fb3
Benjamin Rüth <benjamin.rueth@tum.de>
2016-03-01 15:27:22 +0100

    added updated cleanup output to postproc

--- Prototypes/PYTHON/NURBSReconstruction/DualContouring/plotting/DC_plotting.py
+++ Prototypes/PYTHON/NURBSReconstruction/DualContouring/plotting/DC_plotting.py
@@ -1,5 +1,6 @@
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 
+
 def plot_surface(axis, quads, verts, color, alpha):
     for q in quads:
         vtx = verts[q]
@@ -32,3 +33,16 @@ def plot_hairs(axis, quads_objs, verts, params, color):
     line.color(color)
     axis.add_collection3d(line)
     #axis.scatter(points_data_x, points_data_y, points_data_z,color='k')
+
+
+def plot_edge(axis, edges, verts, color, linewidth):
+    lines_data = []
+    for e in edges:
+        v0 = verts[e[0]]
+        v1 = verts[e[1]]
+        lines_data.append([v0.tolist(),v1.tolist()])
+
+    line = Line3DCollection(lines_data)
+    line.color(color)
+    line.set_linewidth(linewidth)
+    axis.add_collection3d(line)
--- Prototypes/PYTHON/NURBSReconstruction/DualContouring/plotting/plot_csv_files.py
+++ Prototypes/PYTHON/NURBSReconstruction/DualContouring/plotting/plot_csv_files.py
@@ -12,12 +12,28 @@ reader=csv.reader(open("./dc_verts_coarse.csv","rb"),delimiter=';')
 x=list(reader)
 verts=numpy.array(x).astype('float')
 
+reader=csv.reader(open("./dc_non_manifold_edges.csv","rb"),delimiter=';')
+x=list(reader)
+non_manifolds = numpy.array(x).astype('int')
+
+reader=csv.reader(open("./dc_dimensions.csv","rb"),delimiter=';')
+x=list(reader)
+dims_raw = numpy.array(x).astype('float')
+
+dimensions = {}
+dimensions['xmin']=dims_raw[0,0]
+dimensions['ymin']=dims_raw[0,1]
+dimensions['zmin']=dims_raw[0,2]
+
+dimensions['xmax']=dims_raw[1,0]
+dimensions['ymax']=dims_raw[1,1]
+dimensions['zmax']=dims_raw[1,2]
+
 fig = plt.figure()
 ax = Axes3D(fig)
 
 DC_plotting.plot_surface(axis=ax, quads=quads, verts=verts, color='b', alpha=.5)
-
-dimensions = {'xmin':-170,'xmax':195,'ymin':100,'ymax':340,'zmin':-30,'zmax':120}
+DC_plotting.plot_edge(axis=ax, edges=non_manifolds, verts=verts, color='r', linewidth=5)
 
 x_mean = (dimensions['xmax']-dimensions['xmin'])/2.0
 y_mean = (dimensions['ymax']-dimensions['ymin'])/2.0
@@ -41,7 +57,3 @@ ax.set_zlim3d(z_mean-width*.5, z_mean+width*.5)
 ax.set_aspect('equal')
 
 plt.show()
-
-
-
-
