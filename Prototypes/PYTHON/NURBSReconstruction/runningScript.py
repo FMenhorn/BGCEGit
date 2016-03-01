dcb73f3f42c7b8cbb44cfbc8cd519f5399858f97
EWannerberg <daplff@gmail.com>
2016-03-01 22:15:00 +0100

    minor merge

--- Prototypes/PYTHON/NURBSReconstruction/runningScript.py
+++ Prototypes/PYTHON/NURBSReconstruction/runningScript.py
@@ -47,8 +47,8 @@ args = parser.parse_args()
 
 
 print "### Surface Extraction ###"
-#verts_coarse, quads_coarse, verts_fine, parameters = extract_surface(args.path, args.coarsening_factor)
-verts_coarse, quads_coarse, verts_fine, parameters = extract_surface(path, coarsening_factor)
+verts_coarse, quads_coarse, verts_fine, parameters = extract_surface(args.path, args.coarsening_factor)
+#verts_coarse, quads_coarse, verts_fine, parameters = extract_surface(path, coarsening_factor)
 vertices, quads, fine_vertices, new_vertex_list, edges, quad_list = quad_vert_generator(verts_coarse, quads_coarse, verts_fine, parameters)
 
 
@@ -58,14 +58,14 @@ A, B1, B2, C, regularPoints = dualCont_to_ABC_simpl(quad_list, new_vertex_list)
 print "### DooSabin DONE ###"
 
 print "### Peters' Scheme ### "
-#NURBSMatrix, NURBSIndices = fit_NURBS(A, B1, B2, C, regularPoints, vertices, quads, fine_vertices, parameters, args.fairnessWeight)
-NURBSMatrix, NURBSIndices = fit_NURBS(A, B1, B2, C, regularPoints, vertices, quads, fine_vertices, parameters, fairnessWeight)
+NURBSMatrix, NURBSIndices = fit_NURBS(A, B1, B2, C, regularPoints, vertices, quads, fine_vertices, parameters, args.fairnessWeight)
+#NURBSMatrix, NURBSIndices = fit_NURBS(A, B1, B2, C, regularPoints, vertices, quads, fine_vertices, parameters, fairnessWeight)
 print "### Peters' Scheme DONE### "
 
 # TODO: nonchanging_file_name should be a zero string if not provided by the user
 
 print "### Generating Step File ###"
-#export_step( NURBSIndices, NURBSMatrix, args.input_file_name, args.output_file_name, nonchanging_file_name, allowed_domains_file_name)
-export_step( NURBSIndices, NURBSMatrix, input_file_name, output_file_name, nonchanging_file_name, allowed_domains_file_name)
+export_step( NURBSIndices, NURBSMatrix, args.input_file_name, args.output_file_name, nonchanging_file_name, allowed_domains_file_name)
+#export_step( NURBSIndices, NURBSMatrix, input_file_name, output_file_name, nonchanging_file_name, allowed_domains_file_name)
 print "### Step File DONE### "
 
